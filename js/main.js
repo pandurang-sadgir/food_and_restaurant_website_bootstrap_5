aab59034a6c4142d649dabf023202281c30906d1
pandurang sadgir <psadgir111@gmail.com>
2023-09-28 10:05:07 +0530

    couter code changed

--- js/main.js
+++ js/main.js
@@ -26,21 +26,28 @@
 
 
 // Counter logic new
-const valueDisplay = document.querySelectorAll('.count');
+const objectOfElement = document.querySelectorAll('.count');
 const interval = 5000
 
-valueDisplay.forEach((valueDisplay) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+objectOfElement.forEach((element) => {
+    // let startValue = 0;
+    let endValue = parseInt(element.getAttribute("data-val"));
+    let startValue = parseInt(element.getAttribute("data-startValue"));
     // console.log(endValue);
     let duration = Math.floor(interval/endValue);
     // It defines duration within that our count should complete
     let counter = setInterval(()=>{
-        startValue = startValue+1;
-        valueDisplay.textContent = startValue; 
+        endValue>startValue?startValue+=1:startValue-=1;
+        // startValue = startValue+1;
+        element.textContent = startValue; 
         if(startValue === endValue){
             clearInterval(counter);
         }
     },duration);
 
   });
+
+
+
+
+  
